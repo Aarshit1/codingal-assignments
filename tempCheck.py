3f961e0e754f31a77326fa25ddf71f23c9e07a48
Aarshit1 <aarshit.khanal@gmail.com>
2026-04-16 20:54:28 +0545

    Add files via upload

---
+++ tempCheck.py
@@ -0,0 +1,6 @@
+temperature = int(input("Enter the temperature in °C: "))
+
+if temperature > 25:
+    print("Wear light and soft clothes.")
+else:
+    print("Wear warm clothes like a jacket or pullover.")
