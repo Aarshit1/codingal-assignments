86d803b2bcc45d3faa5677ea64daaa16f31a803f
Aarshit1 <aarshit.khanal@gmail.com>
2026-05-10 18:00:52 +0545

    Add files via upload

---
+++ nested_for_class10enrol.py
@@ -0,0 +1,14 @@
+i=int(input("Enter your age to enroll : "))
+
+if i>=10:
+    if i<=20:
+        print("welcome to class 10")
+    if i>20:
+        print("you are too old")
+
+elif i<10:
+    print("you are too young")
+    
+
+else :
+    print("invalid")
